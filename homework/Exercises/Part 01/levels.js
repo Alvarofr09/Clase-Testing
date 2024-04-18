a1d79c50d5d7ca425430ca4023f0e720529de8c6
Alvaro Frias <alvarofriasruiz03@gmail.com>
2024-04-18 14:03:38 +0200

    Hacer funciones

--- homework/Exercises/Part 01/levels.js
+++ homework/Exercises/Part 01/levels.js
@@ -1,8 +1,30 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {
+	return a + b;
+};
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+	if (letras.length < 2) {
+		return letras;
+	}
 
-const levelThree = (a, b) => {};
+	if (letras.length === 2) {
+		return letras[0];
+	}
+
+	if (letras.length > 2) {
+		let result = "";
+		for (let i = 0; i < letras.length; i++) {
+			if (i % 2 === 0) {
+				result += letras[i];
+			}
+		}
+		return result;
+	}
+};
+
+const levelThree = (a, b) => {
+	return a.concat(b).sort((a, b) => a - b);
+};
 
 const levelFour = (num) => {};
 
